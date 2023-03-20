ced3d7146e724d23f2c5d7ec28e5b0bbbb7458ab
PedroV1dal <pedro.vidal2608@gmail.com>
2023-03-20 17:36:45 -0300

    alteracoes na pagina

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,5 +1,11 @@
-export function Avatar(props) {
+import styles from './Avatar.module.css'
+
+export function Avatar({hasBorder = true, src}) {
+
   return (
-    <img className={styles.avatar} src={props.src} />
+    <img 
+      className={hasBorder ? styles.avatarWithBorder : styles.avatar} 
+      src={src} 
+    />
   )
 }
